fae8317e195b80f174e9dbece7d6e113ea278a44
Donavyn <donavynpence@yahoo.com>
2023-04-03 14:43:53 -0600

    placed items for userClicks

--- src/components/RenderImage.js
+++ src/components/RenderImage.js
@@ -1,6 +1,9 @@
 const RenderImage = ({ img, onClick }) => {
   return (
     <div className="imgContainer">
+      <div className="hat"></div>
+      <div className="nose"></div>
+      <div className="left-foot"></div>
       <img src={img} alt={"search board"} onClick={onClick} />
     </div>
   );
